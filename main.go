1484f4cc8d4cfa4605216ec0a54fdd004f80b9ef
Stefan Beeman <stefan@docmunch.com>
2014-02-10 21:59:05 -0800

    Yay, Worms

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/stefanbeeman/antfarm"
 )
 
 func main() {
 	world := antfarm.MakeWorld(".", 10, 10, 1)
-	//world.Show()
-	//world.Run(1000)
-	fmt.Println(world.)
+	world.Run(10000)
 }
