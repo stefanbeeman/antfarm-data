bdfe95e3732124813889be469a621716cd3dfb2b
Stefan Beeman <stefan@docmunch.com>
2014-02-24 10:52:01 -0800

    Update

--- main.go
+++ main.go
@@ -4,17 +4,6 @@ import (
 	"github.com/stefanbeeman/antfarm"
 )
 
-type Stack interface {
-	foo()
-}
-
-type Mack interface {
-	Stack
-	goo()
-}
-
-type StackMack struct{}
-
 func main() {
 	w := af.MakeWorld(".", 20, 20, 1)
 	w.Start()
