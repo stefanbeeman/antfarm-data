98e0141836285936448383fd1d9621c8b49b12c1
Stefan Beeman <stefan@docmunch.com>
2014-03-09 19:58:33 -0700

    .

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/stefanbeeman/antfarm"
 )
 
 func main() {
+	fmt.Println("Starting...")
 	game := antfarm.MakeGame(".", 20, 20, 1)
-	game.Start()
+	game.StartShell()
 }
